99a5a8fb1e5c0b072e5095e7b17ab1e5a16a3384
Rapolas Gruzdys/Trainee <Rapolas.Gruzdys@telia.lt>
2017-12-06 16:26:01 +0200

    Fixed tables, added migrations, comments, category by tables

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Laravel\Scout\Searchable;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use Searchable;
+
+    /**
+     * Get the indexable data array for the model.
+     *
+     * @return array
+     */
+    public function toSearchableArray()
+    {
+        $array = $this->toArray();
+
+        // Customize array...
+
+        return $array;
+    }
+}
