84f7a68a608115ee6ded18d44be76b743658369d
Rapolas Gruzdys/Trainee <Rapolas.Gruzdys@telia.lt>
2017-12-28 13:53:10 +0200

    Fixed users edit with access levels

--- resources/views/user_edit.blade.php
+++ resources/views/user_edit.blade.php
@@ -3,6 +3,7 @@
     <link rel="stylesheet" href="{{ URL::asset('css/categories_edit.css') }}" />
     <div class="container">
         <div class="row">
+            @if($user->email == Auth::user()->email || Auth::user()->access_level == 10)
             <div class="col-md-7">
                 <form action="{{ route('userUpdate', ['user_id' => $user->id]) }}" method="POST">
                     {{ csrf_field() }}
@@ -12,7 +13,7 @@
                     <input type="text" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" name="email" maxlength="50" value="{{$user->email}}" required><br>
                     <label for="name">Username</label>
                     <input type="text" class="form-control" id="name" name="name" placeholder="Enter username" value="{{$user->name}}" required><br>
-                    @if($user->access_level == 10)
+                    @if(Auth::user()->access_level == 10)
                     <label for="access">Access level</label>
                     <select id="access" class="form-control" name="access" required>
                         <option selected>{{$user->access_level}}</option>
@@ -35,6 +36,9 @@
             <button class="btn btn-default" onclick="window.location='{{ URL::previous() }}'">Cancel</button>
                 </span>
             </div>
+                @else
+            You have no permission to see this
+                @endif
         </div>
     </div>
 @endsection
